8a610f5b5e8e4f1143ba0667f5bc3227c75bb630
Ev Schmitt <evanjschmitt@gmail.com>
2025-02-09 21:59:44 -0500

    HW02: Init * Added Project Files * Added .gitignore to prevent MAC file transfer * UP NEXT: Project completion

---
+++ hw02/bin/index.js
@@ -0,0 +1,12 @@
+const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What would you like to name your project? ", (projectName => {
+    
+}))
---
+++ hw02/readme.md
@@ -0,0 +1,5 @@
+# HW02: Custom CLI Tool
+
+## Purpose:
+
+The purpose of this project is to create and publish a CLI tool to NODE
