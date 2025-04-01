b01b2a73671ad4ace89053625595ed1766f2198d
Ev Schmitt <evanjschmitt@gmail.com>
2025-04-01 16:14:05 -0400

    Week12 Init

---
+++ week12/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'views')));
+
+
+const port = process.env.PORT || 14749;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} http://localhost:${port}`);
+}
+);
